66c3d1a2a29c717014f2f407c268567726968f6a
hhyoo84-svg <hhyoo84@gmail.com>
2026-08-27 16:17:50 +0900

    Refactor main.tsx to remove HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom' // 추가
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <HashRouter> {/* App을 HashRouter로 감싸기 */}
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
